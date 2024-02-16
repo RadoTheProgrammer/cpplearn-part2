bf1def4c7a1e3a142c2094d30906434e6ed7c0b8
RadoTheProgrammer <rado@arazakar.com>
2024-02-16 12:00:33 +0100

    finished chap13

---
+++ chap13_class1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Personnage.h"
+
+using namespace std;
+
+int main() {
+    Personnage david, goliath;
+    goliath.attaquer(david);
+    david.boirePotionDeVie(20);
+    goliath.attaquer(david);
+    david.attaquer(goliath);
+    goliath.changerArme("Double hache tranchante vénéneuse de la mort", 40);
+    goliath.attaquer(david);
+
+    return 0;
+}
