9f7634413127b7f00855bb2791465be5b6297e32
RadoTheProgrammer <rado@arazakar.com>
2024-03-02 19:47:58 +0100

    finished chap14

---
+++ chap13_class1/Arme.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Arme
+{
+    public:
+
+    Arme();
+    Arme(std::string nom, int degats);
+    void changer(std::string nom, int degats);
+    void afficher() const;
+    int getDegats() const;
+
+    private:
+
+    std::string m_nom;
+    int m_degats;
+};
---
+++ chap13_class1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Personnage.h"
+
+using namespace std;
+
+int main(){
+    Personnage david, goliath("Épée aiguisée", 20);
+
+    goliath.attaquer(david);
+    david.boirePotionDeVie(20);
+    goliath.attaquer(david);
+    david.attaquer(goliath);
+
+    goliath.changerArme("Double hache tranchante", 40);
+    goliath.attaquer(david);
+    
+    cout << "David" << endl;
+    david.afficherEtat();
+    cout << endl << "Goliath" << endl;
+    goliath.afficherEtat();
+
+    cout << "David est vivant" << david.estVivant() << endl;
+
+    return 0;
+}
