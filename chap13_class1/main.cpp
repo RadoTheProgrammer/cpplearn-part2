cbe2e7a9d1240fdb4460992a891139d38b74f46a
RadoTheProgrammer <rado@arazakar.com>
2024-03-29 11:10:15 +0100

    some commit

--- chap13_class1/main.cpp
+++ chap13_class1/main.cpp
@@ -3,33 +3,33 @@
 
 using namespace std;
 
-// int main(){
-//     Personnage david,goliath("Épée aiguisée", 20);
-
-//     cout << "Hello World" << endl;
-//     goliath.attaquer(david);
-//     david.boirePotionDeVie(20);
-//     goliath.attaquer(david);
-//     david.attaquer(goliath);
-
-//     goliath.changerArme("Double hache tranchante", 40);
-//     goliath.attaquer(david);
-    
-//     cout << "David" << endl;
-//     david.afficherEtat();
-//     cout << endl << "Goliath" << endl;
-//     goliath.afficherEtat();
-
-//     return 0;
-// }
-
 int main(){
     Personnage david,goliath("Épée aiguisée", 20);
 
-    david = goliath;
+    cout << "Hello World" << endl;
+    goliath.attaquer(david);
+    david.boirePotionDeVie(20);
+    goliath.attaquer(david);
+    david.attaquer(goliath);
 
+    goliath.changerArme("Double hache tranchante", 40);
+    goliath.attaquer(david);
+    
+    cout << "David" << endl;
     david.afficherEtat();
-
+    cout << endl << "Goliath" << endl;
     goliath.afficherEtat();
+
     return 0;
 }
+
+// int main(){
+//     Personnage david,goliath("Épée aiguisée", 20);
+
+//     david = goliath;
+
+//     david.afficherEtat();
+
+//     goliath.afficherEtat();
+//     return 0;
+// }
